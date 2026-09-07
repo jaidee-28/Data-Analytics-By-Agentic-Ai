282115483a21a7e15b6bf618237a67aea67859c7
Jaideep Panwar <jaideep.panwar.cer25@itbhu.ac.in>
2026-09-07 11:57:42 +0530

    Fix typo in accessing secrets for API key

--- sapp.py
+++ sapp.py
@@ -28,7 +28,7 @@ st.caption("Upload your sales data to get automatic plots and AI-generated sugge
 
 # ---- API key --------------------------------------------------------------
 
-api_key = st.secret["api_key"]
+api_key = st.secrets["api_key"]
 api_kye= os.getenv("api_key")
 
 # ---- upload -----------------------------------------------------------------
